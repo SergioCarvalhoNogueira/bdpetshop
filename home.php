f2ed040fdc06bc424c889c415587254c4037833f
Sergio <Sergio@Sergio-PC>
2014-12-02 15:59:26 -0200

    fiz teste de comentarios

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
     <meta charset="iso-8859-1" />
     <title># PONTO PET - VETERINÁRIA E PET SHOP #</title>
     <link href="css/estilos.css" rel="stylesheet" />
-</head>
+</head><!-- casa feia-->
 <body>
 <?php   include 'conectar.php'; 
 ?>
